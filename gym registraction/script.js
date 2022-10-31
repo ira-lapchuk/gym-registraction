0e0f796c918467d6a3ef0dbccf47480ec80f9fb6
ira-lapchuk <114941166+ira-lapchuk@users.noreply.github.com>
2022-10-31 20:05:25 +0200

    registration form is ready

--- gym registraction/script.js
+++ gym registraction/script.js
@@ -1,73 +1,46 @@
-const client = {}
-//console.log(client);
 
+function showResult() { 
+  const nameEl = document.getElementById("fullN");
+  const nameValue = nameEl.value;
 
-function showResult() {
-  const fullName = document.getElementById("fullN");
-  const fullN = fullName.value;
-}
+  const ageEl = document.getElementById("ageY");
+  const ageValue = ageEl.value;
 
-function showResult() {
-  const age = document.getElementById("ageY");
-  const ageY = age.value;
+  const minAge = 18;
+  const maxAge = 60;
 
-  const minAge = client.age - 18;
-  const maxAge = client.age - 60;
+  const heightEl = document.getElementById("heightM");
+  const heightValue = heightEl.value;
 
-  if (client.age > maxAge) {
-    console.log("Sorry, find another gym");
+  const minHeight = 160;
+  const maxHeight = 195;
+
+  const weightEl = document.getElementById("weightK");
+  const weightValue = weightEl.value;
+
+  const minWeight = 60;
+  const maxWeight = 120;
+
+  if (ageValue > maxAge) {
+    alert("Sorry, find another gym. You are old");
+  } else if (ageValue < minAge) {
+    alert("Sorry, find another gym. You are too young");
+  } else if (heightValue > maxHeight) {
+    alert("Sorry, find another gym. We have a low ceiling for you");
+  }  else if (heightValue < minHeight) {
+    alert("Sorry, find another gym. You will not get to sports simulators")
+  } else if(weightValue > maxWeight){ 
+    alert("Sorry, find another gym. Your weight is too much"); 
+  } else if (weightValue < minWeight) {
+    alert("Sorry, find another gym. Your weight is too low")
+  } else {
+    alert("Welcome. You are registrated!");
   }
+  
+  eraseData();
+} 
 
-  if (client.age < minAge) {
-    console.log("Sorry, find another gym");
-  }
-
-  if (client.age >= minAge && client.age <= maxAge) {
-    console.log("Welcome. You are registrated!");
-  }
-}
-
-function showResult() {
-  const height = document.getElementById("heightM");
-  const heightM = height.value;
-
-  const minHeight = client.height - 160;
-  const maxHeight = client.height -195;
-
-  if (client.height > maxHeight) {
-    console.log("Sorry, find another gym");
-    }
-
-  if (client.height < minHeight) {
-    console.log("Sorry, find another gym");
-    }
- 
-  if (client.height >= minHeight && client.height <= maxHeight){
-    console.log("Welcome. You are registrated!");
-  }
-}
-
-function showResult() {
-  const weight = document.getElementById("weightK");
-  const weightK = weight.value;
-
-  const minWeight = client.weight - 60;
-  const maxWeight = client.weight -120;
-
-  if (client.weight > maxWeight) {
-    console.log("Sorry, find another gym");
-    }
-
-  if (client.weight < minWeight) {
-    console.log("Sorry, find another gym");
-  }
- 
-  if (client.weight >= minWeight && client.weight <= maxWeight){
-    console.log("Welcome. You are registrated!");
-  }
-}
-
-function showResult() {
+function eraseData() { 
   document.getElementById("fullN").value = "";
   document.getElementById("ageY").value = "";
   document.getElementById("heightM").value = "";
